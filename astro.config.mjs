216977a7e3b1a2d7d75840a2e187438a5a843a4d
Jhonatan Salazar <snesjhon@gmail.com>
2026-02-09 13:20:50 -0800

    update: resume additions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,31 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+// import pdf from "astro-pdf";
 
 export default defineConfig({
-  site: 'https://www.snesjhon.com',
+  site: "https://www.snesjhon.com",
   integrations: [
     mdx(),
     tailwind({
       applyBaseStyles: false, // Don't auto-inject Tailwind into all pages
     }),
+    // pdf({
+    //   baseOptions: {
+    //     pdf: {
+    //       scale: 0.93,
+    //       margin: {
+    //         top: "0.1in",
+    //         bottom: "0.1in",
+    //         left: "0.2in",
+    //         right: "0.2in",
+    //       },
+    //     },
+    //   },
+    //   pages: {
+    //     "/resume": true,
+    //   },
+    // }),
   ],
   markdown: {
     syntaxHighlight: false,
